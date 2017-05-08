9b3ed6bc1a5903d4d6738d2df20e28e63916eeba
jlrandulfe <jlrandulfe@gmail.com>
2017-05-08 19:56:38 +0200

    Added a C example for writing and reading FPGA camera component registers

---
+++ camera_test/main.c
@@ -0,0 +1,76 @@
+// Required by hwlib.h 
+#define soc_cv_av
+// Standard libraries
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <inttypes.h>
+// DS-5 libraries. They are placed at <DS-5-path>/embedded/ip/altera/hps/altera_hps/hwlib/include
+//#include "hwlib.h"
+//#include "soc_cv_av/socal/socal.h"
+//#include "soc_cv_av/socal/hps.h"
+//#include "soc_cv_av/socal/alt_gpio.h"
+// Local project header.
+#include "hps_0.h"
+#include "avalon_camera.h"
+
+#define HPS_FPGA_BRIDGE_BASE 0xC0000000
+#define HW_REGS_BASE ( HPS_FPGA_BRIDGE_BASE )
+#define HW_REGS_SPAN ( 0x04000000 )
+#define HW_REGS_MASK ( HW_REGS_SPAN - 1 )
+
+int main(int argc, char **argv) {
+
+    void *virtual_base;
+    int fd;
+    void *camera_virtual_address;
+    void *dipsw_virtual_address;
+
+    // map the address space for the LED registers into user space so we can interact with them.
+    // we'll actually map in the entire CSR span of the HPS since we want to access various registers within that span
+
+    if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) {
+        printf( "ERROR: could not open \"/dev/mem\"...\n" );
+        return( 1 );
+    }
+
+    virtual_base = mmap( NULL, HW_REGS_SPAN, ( PROT_READ | PROT_WRITE ), MAP_SHARED, fd, HW_REGS_BASE );
+
+    if( virtual_base == MAP_FAILED ) {
+        printf( "ERROR: mmap() failed...\n" );
+        close( fd );
+        return( 1 );
+    }
+    
+    camera_virtual_address = virtual_base + ( ( unsigned long  )( AVALON_CAMERA_BASE ) & ( unsigned long)( HW_REGS_MASK ) );
+
+    dipsw_virtual_address = virtual_base + ( ( unsigned long  )( DIPSW_PIO_BASE ) & ( unsigned long)( HW_REGS_MASK ) );
+    // Write some random value to the exposure register.
+
+    printf("exposure: %u\n", ((unsigned int) IORD_CAMERA_EXPOSURE(camera_virtual_address)));
+    (IOWR_CAMERA_EXPOSURE(camera_virtual_address, 0x2300));
+    // The switches value is printed every second.
+    while(1){
+        printf("Switches: %u\n", (*((unsigned int *) dipsw_virtual_address)));
+        printf("exposure: %u\n", ((unsigned int) IORD_CAMERA_EXPOSURE(camera_virtual_address)));
+        // wait 1s.
+        usleep( 1000*1000 );
+    }
+    
+
+
+    
+
+    // clean up our memory mapping and exit
+    
+    if( munmap( virtual_base, HW_REGS_SPAN ) != 0 ) {
+        printf( "ERROR: munmap() failed...\n" );
+        close( fd );
+        return( 1 );
+    }
+
+    close( fd );
+
+    return( 0 );
+}
