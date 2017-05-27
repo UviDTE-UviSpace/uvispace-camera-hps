4c1bdcd775993650bd13e5cab23b5d2a4bc6b963
robertofem <robertofem@gmail.com>
2017-05-27 03:19:31 +0200

    add capture_get_image function to avalon_camera.hpp this way the user can acquire a image with only one line of code. The return of the function has been also reformated. Now all functions return code errors only.

--- inc/avalon_camera.hpp
+++ inc/avalon_camera.hpp
@@ -5,6 +5,7 @@
 #define __AVALON_CAMERA_H
 
 #include <inttypes.h>//for uint16_t
+#include <string.h>//for memcpy
 
 /* 
 Macros for accessing an address map with a data bus size of 8, 16 or 32 bits.
@@ -91,17 +92,23 @@ private: //accesible only inside the class
     //--Class Variables--//
     //Virtual base address of the avalon_camera. Filled in the constructor
     void* address;
-public:
+
     //Size of the image in pixels (screen pixels. 
     //dont missunderstand with cam pixels that are usually double)
     //Filled each time config_set_width and config_set_height are called
     uint16_t img_width;
     uint16_t img_height;
-private:
+
     //virtual addresses of the buffers where images are captured.
-    cpixel* buff0_v; //buffer to save odd lines
-    cpixel* buff1_v; //buffer to save even lines
-    cpixel* current_buff_v; //sometimes buff0_v, sometimes buff1_v 
+    //virtual and physical addresses of the buffer that connects
+    //hardware. Used to save lines of the image
+    void* buff_v; 
+    void* buff_p; 
+    cpixel* buff0_v; //pointer inside buff_v, to save odd lines 
+    cpixel* buff1_v; //pointer inside buff_v, to save even lines
+    //current_buff_v is sometimes buff0_v, sometimes buff1_v, depending
+    //on the image line currently being acquired
+    cpixel* current_buff_v; 
     
 public: //accessible from outside the class
     //--Class Methods definition--//
@@ -139,10 +146,13 @@ public: //accessible from outside the class
     uint16_t config_get_exposure(void);
     
     //methods to capture an image into the processor
-    int capture_start(void* buffer_v, void* buffer_p);
-    cpixel* capture_get_line(void);
+    int capture_set_buffer(void* buffer_v, void* buffer_p);
+    int capture_get_image(cpixel* image);
+private:
+    int capture_start(void);
+    int capture_get_line(cpixel*& line);
     
-private: //accesible from ouside the class   
+private: //not accesible from ouside the class   
     //resets and removes soft reset to reset the video stream 
     //it is private. not intended to be used by the user yet
     int reset(void);
@@ -265,43 +275,121 @@ private: //accesible from ouside the class
  }
  
  //--Capture image methods
+ //capture_set_buffer
+ //
+ //description: sets the physical and virtual addresses of the hardware
+ //buffer connecting hardware and software. This buffer is used to save
+ //2 lines of the image. Since this buffer may not have enough space to
+ //store a whole image (in example, 4 8-bit components of a 640x480 image
+ //occupies 1.2MB while HPS-OCR has only 64kB) while one line is being 
+ //acquired by the hardware component capture_image, the other is being 
+ //copied to a buffer in processor RAM with enough space to store a whole
+ //image.
+ //
+ //parameters:
+ //-buffer_v: virtual address of the buffer to connect processor and fpga.
+ // This buffer must have enough space to store 2 lines of the image,
+ // that is: Minimum size = sizeof(cpixel)*this->img_width*2.
+ // This buffer must be physically contiguous cause the hardware component
+ // image_capture in will write continuously to it.
+ //-buffer_p: equivalent physical address for the buffer.
+ //
+ //return: 0 if success
+ int Camera::capture_set_buffer(void* buffer_v, void* buffer_p)
+ {
+    this->buff_v = buffer_v;
+    this->buff_p = buffer_p;
+    return 0;
+ }
+ 
+ //capture_get_image
+ //
+ //description
+ //After configuring the camera and calling capture_set_buffer
+ //the user can call this function to retrieve a whole image
+ //from the video stream in the fpga. This function uses
+ //capture_start() to start a new capture. Then uses capture_get_line()
+ //to know when a new line has been acquired. When a new line is 
+ //acquired the processor copies its content into the image buffer,
+ //passed as an argument to this function. When all lines are aquired
+ //the function ends.
+ //
+ //parameters: image is a buffer with enough space to store a whole image.
+ //            The image is stored by lines in this buffer, that is 
+ //            line0 goes first in the buffer, then line1 and so on. 
+ //
+ //return: 0 success
+ //        1 error starting the capture (component does not answer
+ //          (it is in reset or not connected to the bus).
+ //        2 error line already acquired when entering capture_get_line.
+ //          Possible loss of data.
+ //          The processor did too much in between two calls to 
+ //          capture_get_line. Try to reduce frame rate.
+ //        3 Too much waiting for a new line. Looks like no video 
+ //          stream is there. Maybe the video stream is in reset state?
+ int Camera::capture_get_image(cpixel* image)
+ {
+    int error;
+    int i; 
+    cpixel* line;
+    int line_size_B = this->img_width * sizeof(cpixel); //line size in Bytes
+    
+    //Start capture
+    error = this->capture_start();
+    if (error == 1) return 1;
+    
+    //Capture lines 
+    for(i=0; i<this->img_height; i++) //for every line
+    {
+        //wait till line is captured
+        error = this->capture_get_line(line);
+        if (error == 1)
+        {
+            printf("Error in line %d\n", i);
+            return 2;
+        }
+        else if (error == 2) return 3;
+        
+        //Copy the line from buffer for lines to big buffer in processor
+        //memcpy is the fastest method to copy data (for dma a driver is needed)
+        //void * memcpy ( void * destination, const void * source, size_t num );
+        memcpy ((void*)&image[i*this->img_width], (void*)line, line_size_B );
+    }
+    return 0;
+ }
+     
  //capture_start()
  //
- //description: starts the acquisition of an image using image_capture 
- //component. image_capture component uses 2 pointers (buff0 and buff1) to
- //save lines of the image. The buffer passed in this function should
- //have space enough to store 2 lines = sizeof(cpixel)*this->img_width*2.
- //image_capture will use the first part of the buffer to save odd lines
- //(buff0) and the second to save even lines (buff1). After capture_start
- //is called use capture_get_line() to know when a new line has been
- //captured and its virtual address to get access from processor to the 
- //line. The number of lines that will be acquired are this->img_height.
- //There is no need to wait if you wanna stop a capture in the middle.
+ //description: 
+ //starts the acquisition of an image using image_capture 
+ //component. After capture_start is called use capture_get_line() to know 
+ //when a new line has been captured and where it is stored.  
+ //This function is "recall-safe". There is no need to wait until the end 
+ //of the capture to call capture_start() again. 
  //The function will wait until the component enters in standby to
- //synchronize and start a new image. So it is safe to recall the function
- //even if you don´t know the state of the previous acquisition. 
+ //synchronize and start a new image from the beginning. So it is safe 
+ //to recall the function even if you don´t know the state of the previous 
+ //acquisition. 
  //
- // parameters:
- //   -buffer_v: Software address of the buffer where lines will
- //    be stored. 
- //   -buffer_p: Equivalent physical address to buffer_v
- //    This buffer must be physically contiguous cause image_capture will
- //    write continuously to it.
- //    Minimum size = sizeof(cpixel)*this->img_width*2.
  //
- // return: 0 if success. -1 if there was some error.
- int Camera::capture_start(void* buffer_v, void* buffer_p)
+ // return: 0 success. 
+ //         1 error starting the capture (component does not answer
+ //          (it is in reset or not connected to the bus).
+ 
+ int Camera::capture_start(void)
  {
     //generate the software addresses of buff0 and buff1
-    this->buff0_v = (cpixel*) buffer_v;
-    this->buff1_v = (cpixel*)((uint8_t*)buffer_v + sizeof(cpixel)*this->img_width);
+    this->buff0_v = (cpixel*) this->buff_v;
+    this->buff1_v = (cpixel*)((uint8_t*)this->buff_v 
+                    + sizeof(cpixel)*this->img_width);
     //First line will be saved in buff0
     this->current_buff_v = this->buff0_v; 
     
     //generate physical addresses and save the into the avalon_camera
     //so image capture knows physical addresses of buff0 and buff1
-    void* buff0_p = buffer_p;
-    void* buff1_p = (void*)((uint8_t*)buffer_p + sizeof(cpixel)*this->img_width);
+    void* buff0_p = this->buff_p;
+    void* buff1_p = (void*)((uint8_t*)this->buff_p 
+                    + sizeof(cpixel)*this->img_width);
     IOWR32(this->address, CAMERA_BUFF0, buff0_p);
     IOWR32(this->address, CAMERA_BUFF1, buff1_p);
     
@@ -317,12 +405,13 @@ private: //accesible from ouside the class
         //ugly way avoid software to get stuck
         counter--; 
     }
-    if (counter == 0) return -1;
+    if (counter == 0) return 1;
     
-    //Now the component is in STandby (state1). Counters and full buffer 
+    //Now the component is in Standby (state1). Counters and full buffer 
     //in image_capture component flags are automatically reset in hardware.
     //Reset the registers saving rising edges of buff0full and buff1full
-    //in avalon camera.
+    //in avalon camera, just in case the previous capture finished in
+    //wrong way.
     IOWR32(this->address, CAMERA_BUFF0_FULL, 0);
     IOWR32(this->address, CAMERA_BUFF1_FULL, 0);
     
@@ -332,25 +421,32 @@ private: //accesible from ouside the class
     
     return 0;
  }
+ 
  //capture_get_line()
  //
- //description: waits until the current buffer is acquired with a new line.
- //it gives as return value the pointer of the line just acquired.
- //This buffer should be emptied before the next line is acquired otherwise
- //new data will overwrite the old line. The function checks just after
- //entering the flag of the line that is being acquired. 
+ // description: 
+ // waits until the current buffer is acquired with a new line.
+ // it gives as return value the pointer of the line just acquired.
+ // This buffer should be emptied before the next line is acquired otherwise
+ // new data will overwrite the old line. The function checks just after
+ // entering the flag of the line that is being acquired. 
  //
- // return: pointer of the image line just acquired. 0 if there the line
- // was already acquired when entering in the function (without any waiting).
- // returns 1 if there was excesive waiting.
- cpixel* Camera::capture_get_line(void)
+ // parameters: line is a pointer to the buffer where image_capture component
+ //             just acquired the current line. It is an output parameter passed
+ //             byref using &.
+ //
+ // return: 0 success
+ //         1 the line was already acquired when entering in the function 
+ //         (without any waiting).
+ //         2 there was excesive waiting on the line.
+ int Camera::capture_get_line(cpixel*& line)
  {
     int counter = 10000000;
     //if the camera is now saving in the buff0 (odd lines)
     if (this->current_buff_v == this->buff0_v)
     {
         //check if buff0 is full without waiting 
-        if ((IORD32(this->address, CAMERA_BUFF0_FULL)) == 1) return 0;
+        if ((IORD32(this->address, CAMERA_BUFF0_FULL)) == 1) return 1;
         else
         { 
             //wait for the line to be acquired
@@ -359,18 +455,19 @@ private: //accesible from ouside the class
                 //ugly way avoid software to get stuck
                 counter--; 
             }
-            if (counter == 0) return (cpixel*)1;
+            if (counter == 0) return 2;
             
             IOWR32(this->address, CAMERA_BUFF0_FULL, 0); //reset the flag
             this->current_buff_v = this->buff1_v;//change the acquisition buffer
-            return this->buff0_v; //return address of the current acquired line         
+            line = this->buff0_v; //return address of the current acquired line     
+            return 0;
         }
     }
     //if the camera is now saving in the buff1 (even lines)
     else
     {
         //check if buff1 is full without waiting 
-        if ((IORD32(this->address, CAMERA_BUFF1_FULL)) == 1) return 0;
+        if ((IORD32(this->address, CAMERA_BUFF1_FULL)) == 1) return 1;
         else
         {
             //wait for the line to be acquired
@@ -379,11 +476,12 @@ private: //accesible from ouside the class
                 //ugly way avoid software to get stuck
                 counter--; 
             }
-            if (counter == 0) return (cpixel*)1;
+            if (counter == 0) return 2;
             
             IOWR32(this->address, CAMERA_BUFF1_FULL, 0); //reset the flag
             this->current_buff_v = this->buff0_v;//change the acquisition buffer
-            return this->buff1_v; //return address of the current acquired line         
+            line = this->buff1_v; //return address of the current acquired line
+            return 0;            
         }
     }
  }
@@ -396,5 +494,4 @@ private: //accesible from ouside the class
     IOWR32(this->address, CAMERA_SOFT_RESET, 1);//remove reset
     return 0; //return 0 on success
  }
-
 #endif //__AVALON_CAMERA_H
