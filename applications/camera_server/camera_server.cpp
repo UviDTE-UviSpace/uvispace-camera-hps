26c99a9b547d1bebd630acd503744ac9b2279adf
floonone <floonone@users.noreply.github.com>
2017-11-14 16:53:46 +0100

    Update camera_server to work with multiple image_types

--- applications/camera_server/camera_server.cpp
+++ applications/camera_server/camera_server.cpp
@@ -1,6 +1,22 @@
 #include "camera_server.hpp"
 
-camera_server::camera_server::camera_server(int port) : abstract_server(port) {
+camera_server::camera_server::camera_server(int port, int image_type) : abstract_server(port) {
+    // Set up sysfs variables
+    std::ofstream image_type_configuration;
+    image_type_configuration.open("/sys/uvispace_camera/attributes/image_type");
+    if (!image_type_configuration.is_open()) {
+        throw server_error::server_init_error("could not set up uvispace_camera");
+    }
+    image_type_configuration << image_type;
+    image_type_configuration.close();
+
+    // Store pixel size
+    if (image_type == 0) {
+      this->pixel_size = 4;
+    } else if ((image_type == 1) || (image_type == 2)) {
+      this->pixel_size = 1;
+    }
+
     // Open camera device
     this->uvicamera.open("/dev/uvispace_camera", std::ios::binary);
     if (!this->uvicamera.is_open()) {
@@ -20,7 +36,8 @@ std::string camera_server::camera_server::process_request(std::string request) {
 }
 
 std::string camera_server::camera_server::capture_frame() {
-    std::string result(IMAGE_MEM, '\0');
-    this->uvicamera.read(&result[0], IMAGE_MEM);
+    std::uint32_t image_size = IMAGE_HEIGHT * IMAGE_WIDTH * this->pixel_size;
+    std::string result(image_size, '\0');
+    this->uvicamera.read(&result[0], image_size);
     return result;
 }
--- applications/camera_server/main.cpp
+++ applications/camera_server/main.cpp
@@ -1,7 +1,33 @@
 #include "main.hpp"
 
-int main() {
-    camera_server::camera_server cs(36000);
+int main(int argc, char** argv) {
+    // Process command line arguments
+    if (argc != 2) {
+      std::cout << "Usage:\n";
+      std::cout << "camera_server --binary\n";
+      std::cout << "camera_server --greyscale\n";
+      std::cout << "camera_server --rgbg\n";
+      return 1;
+    }
+
+    std::string image_type_argument(argv[1]);
+    int image_type;
+    if (image_type_argument == "--rgbg") {
+      image_type = 0;
+    } else if (image_type_argument == "--greyscale") {
+      image_type = 1;
+    } else if (image_type_argument == "--binary") {
+      image_type = 2;
+    } else {
+      std::cout << "Usage:\n";
+      std::cout << "camera_server --binary\n";
+      std::cout << "camera_server --greyscale\n";
+      std::cout << "camera_server --rgbg\n";
+      return 1;
+    }
+
+    // Run server
+    camera_server::camera_server cs(36000, image_type);
     cs.run();
     return 0;
 }
