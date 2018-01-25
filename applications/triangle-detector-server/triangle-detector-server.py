0c18708337ccd05fa8b5aad0b517f28b961d1260
Roberto Fernandez <robertofem@gmail.com>
2018-01-25 21:09:29 +0100

    modify triangle detector server to publish gray and bin image and triangles everything is published. It doesnt waist ethernet bandwith if there is no client connected and the Cyclone V SoC can go at maximum frames per second the hardware can provide so no problem.

--- applications/triangle-detector-server/triangle-detector-server.py
+++ applications/triangle-detector-server/triangle-detector-server.py
@@ -5,28 +5,35 @@ import zmq
 
 import _find_contours
 import _polygon
-
+# This server posts in 3 different zmq sockets the gray image, the binary image and the
+# triangles (position of cars) obtained from the binary image.
 
 def main():
-    # Configure image type as binary
-    with open("/sys/uvispace_camera/attributes/image_type", "w") as f:
-        f.write("2")
-    # Init fps counter
+    # Init fps(frames per second) counter
     mean_fps = 0
     # Bind publisher socket
-    frame_publisher = zmq.Context.instance().socket(zmq.PUB)
+    bin_frame_publisher = zmq.Context.instance().socket(zmq.PUB)
+    gray_frame_publisher = zmq.Context.instance().socket(zmq.PUB)
     triangle_publisher = zmq.Context.instance().socket(zmq.PUB)
-    frame_publisher.sndhwm = 1
+    #Make the zmq fuffer equal to 1 so when client asks for data only gives last image.
+    #Otherwise with a slow client images would go accumulating in zmq buffers produccing
+    #a delay in the stream.
+    bin_frame_publisher.sndhwm = 1
+    gray_frame_publisher.sndhwm = 1
     triangle_publisher.sndhwm = 1
-    frame_publisher.bind("tcp://*:33000")#for image
-    triangle_publisher.bind("tcp://*:5005")#for triangles
-    f =  open("/dev/uvispace_camera", "rb");
+    bin_frame_publisher.bind("tcp://*:33000")
+    gray_frame_publisher.bind("tcp://*:33001")
+    triangle_publisher.bind("tcp://*:5005")
+    f_bin  =  open("/dev/uvispace_camera_bin",  "rb");
+    f_gray =  open("/dev/uvispace_camera_gray", "rb");
     while True:
         t1 = datetime.datetime.now()
-        frame = numpy.fromfile(f, numpy.uint8, 480 * 640).reshape((480, 640))
-        triangles = process_frame(frame)
+        bin_frame = numpy.fromfile(f_bin, numpy.uint8, 480 * 640).reshape((480, 640))
+        triangles = process_frame(bin_frame)
         triangle_publisher.send_json(triangles)
-        frame_publisher.send(frame)
+        bin_frame_publisher.send(bin_frame)
+        gray_frame = numpy.fromfile(f_gray, numpy.uint8, 480 * 640).reshape((480, 640))
+        gray_frame_publisher.send(gray_frame)
         last_fps = 1000000 / (datetime.datetime.now() - t1).microseconds
         mean_fps = 0.9 * mean_fps + 0.1 * last_fps
         print(mean_fps)
